352772a287586d8520b9f153a7f6791060773a61
Ondřej Ticháček <ondrejtichacek@gmail.com>
2021-03-16 13:31:15 +0100

    configs

--- mdshark/config.py
+++ mdshark/config.py
@@ -1,10 +1,76 @@
+import socket
+import re
 import pathlib
 from pathlib import Path
 
-path = {
-    'gmx': Path('/usr/local/gromacs/gromacs-2020.5/bin/gmx'), # location of gmx
-    'mdrun_plumed': Path('/usr/bin/mdrun_plumed'), # location of mdrun_plumed
+default_path = {
+        # gromacs
+        'gmx': 'gmx',
+
+        # gromacs-plumed
+        'mdrun_plumed': 'mdrun_plumed',
+        'plumed': 'plumed',
+    }
+
+host = socket.gethostname()
+if host == 'lucy':
+    path = {
+        # gromacs
+        'gmx': Path('/usr/local/gromacs/gromacs-2020.5/bin/gmx'),
+
+        # gromacs-plumed
+        'mdrun_plumed': Path('/usr/bin/mdrun_plumed'),
+        'plumed': Path('/usr/bin/plumed'),
+    }
+elif (host == 'login1.aurum.ccf.uochb.local' 
+        or re.match(r'ai\d+\.aurum\.ccf\.uochb\.local', host)):
+
+    path = default_path
+
+    path['gmx'] = 'gmx'
+    path['gmx make_ndx'] = 'gmx make_ndx'
+    path['mdrun_plumed'] = 'gmx mdrun'
+    
+    environment_setup = {        
+        'gromacs': [
+            "source /opt/uochb/soft/spack/20210305/share/spack/setup-env.sh",
+            "spack load gromacs",
+            # "source /opt/uochb/soft/spack/20191115/share/spack/setup-env.sh",
+            # "spack env activate gromacs2019",
+            # "spack env activate gromacs2020-nompi",
+            # "source /opt/uochb/soft/spack/latest/share/spack/setup-env.sh",
+            # "spack env activate gromacs2020",
+        ],
+
+        'plumed': [
+            "source /opt/uochb/soft/spack/20210305/share/spack/setup-env.sh",
+            "spack load gromacs",
+            # "source /opt/uochb/soft/spack/20191115/share/spack/setup-env.sh",
+            # "spack env activate gromacs2019-plumed",
+            # "spack env activate gromacs2020-nompi",
+            # "source /opt/uochb/soft/spack/latest/share/spack/setup-env.sh",
+            # "spack env activate gromacs2020",
+        ],
+
+        'gaussian': None, 
+        # 'gaussian': [
+        #     "export g16root=/opt/uochb/soft/gaussian/gaussian16_a03/arch/amd64-pgi_12.10-acml",
+        #     "export LD_LIBRARY_PATH=/opt/uochb/soft/gaussian/gaussian16_a03/arch/amd64-pgi_12.10-acml/g16",
+        #     "export GAUSS_EXEDIR=/opt/uochb/soft/gaussian/gaussian16_a03/arch/amd64-pgi_12.10-acml/g16",
+        #     "export GAUSS_SCRDIR=/dev/shm",
+        #     "source /opt/uochb/soft/anaconda/202007/bin/activate",
+        #     "conda activate sugaroptim",
+        # ]
+    }
+else:
+    raise(ValueError(f"Host {host} not set up."))
+
+
+slurm_additional_parameters = {
+    'qos':'backfill',
+    'reservation': 'ondra',
 }
+
 
 cluster_bash_header = {}
 
